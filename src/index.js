7e966a8d7dce59e615c1398c28ec89a20393cb65
Joshua Kruder <joshua.kruder@gmail.com>
2018-07-01 21:23:18 -0400

    Removed service worker.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,4 @@ import configureStore from './data/configureStore'
 
 const store = configureStore()
 
-// service workers would be neat to explore but are not relevant
-// to work's environment -- the user must be connected at all times
-// import registerServiceWorker from './registerServiceWorker'
-// registerServiceWorker()
-
 ReactDOM.render(<App store={store} />, document.getElementById('root'))
